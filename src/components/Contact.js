6e3672972cc23ac5e40ae7157598d1088726a57a
Jacob Lynch <jacoblynch99@gmail.com>
2021-08-05 11:32:09 -0500

    updated all components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,17 @@ import React, { useState } from 'react'
 const Contact = (props) => {
     return (
         <div>
-            <h1>Contact</h1>
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <CustomButton linkpath={'/'} content={'Home'} />
+                <CustomButton linkpath={'/Contact'} content={'Contact'} />
+                <CustomButton linkpath={'/AboutMe'} content={'About Me'} />
+                <CustomButton linkpath={'/Projects'} content={'Projects'} />
+            </Grid>
         </div>
     )
 }
