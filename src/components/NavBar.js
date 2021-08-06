4e53082824ee48004932712a2bb41aefe469572f
Jacob Lynch <jacoblynch99@gmail.com>
2021-08-06 06:37:08 -0500

    updated

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const NavBar = (props) => {
+    return (
+        <div>
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <CustomButton linkpath={'/'} content={'Home'} />
+                <CustomButton linkpath={'/Contact'} content={'Contact'} />
+                <CustomButton linkpath={'/AboutMe'} content={'About Me'} />
+                <CustomButton linkpath={'/Projects'} content={'Projects'} />
+            </Grid>
+        </div>
+    )
+}
+
+export default NavBar
