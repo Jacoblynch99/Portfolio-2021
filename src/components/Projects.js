de9afdef2d0d14d9574ab377fc8fc02639163975
Jacob Lynch <jacoblynch99@gmail.com>
2021-08-06 06:53:49 -0500

    updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,33 @@
 import React, { useState } from 'react'
 import CustomButton from './CustomButton'
+import {
+    AppBar,
+    Box,
+    Grid,
+    Select,
+    MenuItem,
+    Button,
+    Typography,
+    Card,
+    CardContent,
+    CardActions,
+    Chip,
+    Dialog,
+    DialogTitle,
+    DialogActions,
+    Container,
+} from '@material-ui/core'
 
 const Projects = (props) => {
-    return <div>TESTTT</div>
+    return (
+        <Grid>
+            <Grid></Grid>
+            <Grid></Grid>
+            <Grid></Grid>
+            <Grid></Grid>
+            <Grid></Grid>
+        </Grid>
+    )
 }
 
 export default Projects
